571ede39d682c64ea9b2df4c8d292f0fe235e6a8
Ashish Bitrox <ashish.bitrox@gmail.com>
2025-06-27 18:36:12 +0530

    Fix messages  Issue

--- server/utils/i18n.js
+++ server/utils/i18n.js
@@ -1,9 +1,13 @@
 import { I18n } from "i18n";
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const i18n = new I18n({
     locales: ["en", "sp"],
     defaultLocale: 'en',
-    directory: "./messages",
+    directory: join(__dirname, '../messages'),
     objectNotation: true,
     register: global
 })
